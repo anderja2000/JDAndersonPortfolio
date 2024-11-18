1d3964c52e88ca926ab943535e8ab8cd571a75c7
anderja2000 <70916539+anderja2000@users.noreply.github.com>
2024-11-17 19:17:55 -0600

    Delete Backend directory

--- Backend/RecipeManager.Models/DTOs/IngredientDto.cs
+++
@@ -1,9 +0,0 @@
-namespace RecipeManager.Models.DTOs
-{
-    public class IngredientDto
-    {
-        public required string Name { get; set; }
-        public required string Quantity { get; set; }
-       
-    }
-}
--- Backend/RecipeManager.Models/DTOs/MultipleIngredientsDto.cs
+++
@@ -1,6 +0,0 @@
-namespace RecipeManager.Models.DTOs; 
-public class MultipleIngredientsDto
-{
- public int RecipeId { get; set; }
-    public List<IngredientDto> Ingredients { get; set; }
-}
