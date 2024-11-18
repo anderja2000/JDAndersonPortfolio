8955987f88f0c1bfaf1b50799a86d4948c985845
anderja2000 <70916539+anderja2000@users.noreply.github.com>
2024-11-18 09:10:00 -0600

    added dist

--- Backend/RecipeManager.Data/obj/Debug/net8.0/RecipeManager.Data.AssemblyInfo.cs
+++ Backend/RecipeManager.Data/obj/Debug/net8.0/RecipeManager.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecipeManager.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+141f67f57958f91f64d4848449ccd24220949bd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d4782905a274127db0df6219972f2b55c08f191")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecipeManager.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecipeManager.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
