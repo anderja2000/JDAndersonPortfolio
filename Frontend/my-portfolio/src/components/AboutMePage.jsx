5a6606c9abc05e544fe9300bd6b6f35234bab64b
anderja2000 <70916539+anderja2000@users.noreply.github.com>
2024-11-18 23:36:53 -0600

    about me updated

--- Frontend/my-portfolio/src/components/AboutMePage.jsx
+++ Frontend/my-portfolio/src/components/AboutMePage.jsx
@@ -5,6 +5,7 @@ export default function AboutMePage() {
     return (
         <main className="about-me-page">
             <h1>About Me Page</h1>    
+            <h2>my name</h2>
         </main>
     )
 }
