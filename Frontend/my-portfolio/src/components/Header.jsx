f4c59e6b54971c1dc14e4436ca28c5978915b9e2
anderja2000 <70916539+anderja2000@users.noreply.github.com>
2024-12-04 16:40:24 -0600

    headear changes

--- Frontend/my-portfolio/src/components/Header.jsx
+++ Frontend/my-portfolio/src/components/Header.jsx
@@ -1,5 +1,9 @@
 import { Link } from "react-router-dom";
-import { FaHome, FaUser, FaProjectDiagram, FaTools, FaEnvelope } from "react-icons/fa";
+import { BsFileEarmarkCodeFill } from "react-icons/bs";
+import { GiLightBulb } from "react-icons/gi";
+import { FaTimeline } from "react-icons/fa6";
+
+import { FaHome, FaUser, FaTools, FaEnvelope } from "react-icons/fa";
 import { AiOutlineHistory } from 'react-icons/ai';
 import { useContext, useEffect, useState } from "react";
 import { ThemeContext } from "../context/ThemeContext.jsx";
@@ -14,7 +18,7 @@ const Header = () => {
     useEffect(() => {
         const handleScroll = () => {
             const headerHeight = document.querySelector('.header').offsetHeight;
-            if (window.scrollY > headerHeight / 2) {    
+            if (window.scrollY > headerHeight / 2) {
                 setIsSticky(true);
             } else {
                 setIsSticky(false);
@@ -27,7 +31,7 @@ const Header = () => {
             window.removeEventListener('scroll', handleScroll);
         };
     }, []);
-    
+
     return (
         <>
             {isSticky && <div style={{ height: '5em' }} />}
@@ -39,7 +43,7 @@ const Header = () => {
                             <Link to="/">
                                 <FaHome className="icon" />
                                 <span className="icon-label">Home</span>
-            {/* Spacer div to maintain layout when header becomes sticky */}
+                                {/* Spacer div to maintain layout when header becomes sticky */}
                             </Link>
                         </div>
                     </li>
@@ -54,7 +58,8 @@ const Header = () => {
                     <li>
                         <div className="icon-col">
                             <Link to="/Projects">
-                                <FaProjectDiagram className="icon" />
+                                <BsFileEarmarkCodeFill className="icon" />
+
                                 <span className="icon-label">Projects</span>
                             </Link>
                         </div>
@@ -62,7 +67,7 @@ const Header = () => {
                     <li>
                         <div className="icon-col">
                             <Link to="/Skills">
-                                <FaTools className="icon" />
+                                <GiLightBulb className="icon" />
                                 <span className="icon-label">Skills</span>
                             </Link>
                         </div>
@@ -70,7 +75,7 @@ const Header = () => {
                     <li>
                         <div className="icon-col">
                             <Link to="/CareerTimeline">
-                                <AiOutlineHistory className="icon" />
+                                <FaTimeline  className="icon" />
                                 <span className="icon-label">Career Timeline</span>
                             </Link>
                         </div>
